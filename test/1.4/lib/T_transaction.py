64b74fb388d90f49d0181987f3b3b8412631cd07
Sven Westergren <sven.westergren@intel.com>
2021-12-13 16:37:05 +0100

    Split transaction accesses at 8-byte address boundaries

--- test/1.4/lib/T_transaction.py
+++ test/1.4/lib/T_transaction.py
@@ -40,3 +40,39 @@ for b, endian in ((obj.bank.b, 'little'), (obj.bank.be, 'big')):
                        (Sim_PE_No_Exception, 5, 4712, 3))
     stest.expect_equal(b.iface.transaction.issue(trans, 13),
                        Sim_PE_No_Exception)
+
+    exp_offsets = [17, 24, 32, 0]
+    exp_chunks = [(0, 7), (7, 15), (15, 20)]
+    exp_multi_value = [0x6050403020100, 0xe0d0c0b0a090807, 0x131211100f, 0]
+    data = b"".join(int.to_bytes(v, sz, endian)
+                    for (v, sz) in zip(exp_multi_value, (7, 8, 5)))
+    exp_value = exp_multi_value[2]
+
+    trans = transaction_t(write=True, inquiry=True, data=data)
+    stest.expect_equal((b.iface.transaction.issue(trans, 17),
+                        b.set_offset, b.set_value, b.set_size),
+                       (Sim_PE_No_Exception, 32, exp_value, 5))
+    stest.expect_equal(b.set_multi_value, exp_multi_value)
+    stest.expect_equal(b.set_multi_offset, exp_offsets)
+
+    trans = transaction_t(write=True, data=data)
+    stest.expect_equal((b.iface.transaction.issue(trans, 17),
+                        b.write_offset, b.write_value),
+                       (Sim_PE_No_Exception, 32, exp_value))
+    stest.expect_equal(b.write_multi_value, exp_multi_value)
+    stest.expect_equal(b.write_multi_offset, exp_offsets)
+
+    trans = transaction_t(read=True, inquiry=True, size=20)
+    stest.expect_equal((b.iface.transaction.issue(trans, 17),
+                        b.get_offset, b.get_size),
+                       (Sim_PE_No_Exception, 32, 5))
+    stest.expect_equal(b.get_multi_offset, exp_offsets)
+    for x, y in exp_chunks:
+        stest.expect_equal(int.from_bytes(trans.data[x:y], endian), 4711)
+
+    trans = transaction_t(read=True, size=20)
+    stest.expect_equal((b.iface.transaction.issue(trans, 17), b.read_offset),
+                       (Sim_PE_No_Exception, 32))
+    stest.expect_equal(b.read_multi_offset, exp_offsets)
+    for x, y in exp_chunks:
+        stest.expect_equal(int.from_bytes(trans.data[x:y], endian), 4711)
