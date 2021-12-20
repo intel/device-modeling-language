b4ff442e24bcf4ee76bc803c67749fc45c9ae087
Sven Westergren <sven.westergren@intel.com>
2021-12-20 11:54:09 +0100

    Extend transaction-testing with big-endian bank

--- test/1.4/lib/T_transaction.py
+++ test/1.4/lib/T_transaction.py
@@ -4,34 +4,39 @@
 from simics import Sim_PE_No_Exception
 import stest
 
-b = obj.bank.b
 
-trans = transaction_t(read=True, size=4)
-stest.expect_equal((b.iface.transaction.issue(trans, 3),
-                    b.read_offset, trans.value_le, b.read_mask, b.read_aux),
-                   (Sim_PE_No_Exception, 3, 4711, 0xffffffff, 1234))
-with stest.expect_log_mgr(obj, 'spec-viol'):
+for b, endian in ((obj.bank.b, 'little'), (obj.bank.be, 'big')):
+    trans = transaction_t(read=True, size=4)
+    stest.expect_equal((b.iface.transaction.issue(trans, 3), b.read_offset,
+                        trans.value_be if endian == 'big' else trans.value_le,
+                        b.read_mask, b.read_aux),
+                       (Sim_PE_No_Exception, 3, 4711, 0xffffffff, 1234),
+                       trans.data)
+    with stest.expect_log_mgr(obj, 'spec-viol'):
+        stest.expect_equal(b.iface.transaction.issue(trans, 13),
+                           Sim_PE_IO_Not_Taken)
+
+    trans = transaction_t(read=True, inquiry=True, size=4)
+    stest.expect_equal((b.iface.transaction.issue(trans, 3), b.get_offset,
+                        trans.value_be if endian == 'big' else trans.value_le,
+                        b.get_size),
+                       (Sim_PE_No_Exception, 3, 4711, 4,))
     stest.expect_equal(b.iface.transaction.issue(trans, 13),
                        Sim_PE_IO_Not_Taken)
 
-trans = transaction_t(read=True, inquiry=True, size=4)
-stest.expect_equal((b.iface.transaction.issue(trans, 3),
-                    b.get_offset, trans.value_le, b.get_size),
-                   (Sim_PE_No_Exception, 3, 4711, 4,))
-stest.expect_equal(b.iface.transaction.issue(trans, 13),
-                   Sim_PE_IO_Not_Taken)
+    trans = transaction_t(write=True, data=int.to_bytes(4712, 3, endian))
+    stest.expect_equal((b.iface.transaction.issue(trans, 5),
+                        b.write_offset, b.write_value, b.write_mask,
+                        b.write_aux),
+                       (Sim_PE_No_Exception, 5, 4712, 0xffffff, 1234))
+    with stest.expect_log_mgr(obj, 'spec-viol'):
+        stest.expect_equal(b.iface.transaction.issue(trans, 13),
+                           Sim_PE_IO_Not_Taken)
 
-trans = transaction_t(write=True, data=b'\x21\x43\x00')
-stest.expect_equal((b.iface.transaction.issue(trans, 5),
-                    b.write_offset, b.write_value, b.write_mask, b.write_aux),
-                   (Sim_PE_No_Exception, 5, 0x4321, 0xffffff, 1234))
-with stest.expect_log_mgr(obj, 'spec-viol'):
+    trans = transaction_t(
+        write=True, inquiry=True, data=int.to_bytes(4712, 3, endian))
+    stest.expect_equal((b.iface.transaction.issue(trans, 5),
+                        b.set_offset, b.set_value, b.set_size),
+                       (Sim_PE_No_Exception, 5, 4712, 3))
     stest.expect_equal(b.iface.transaction.issue(trans, 13),
-                       Sim_PE_IO_Not_Taken)
-
-trans = transaction_t(write=True, inquiry=True, data=b'\x11\x22\x33')
-stest.expect_equal((b.iface.transaction.issue(trans, 5),
-                    b.set_offset, b.set_value, b.set_size),
-                   (Sim_PE_No_Exception, 5, 0x332211, 3))
-stest.expect_equal(b.iface.transaction.issue(trans, 13),
-                   Sim_PE_No_Exception)
+                       Sim_PE_No_Exception)
