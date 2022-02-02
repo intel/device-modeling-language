53e3b62549ef0601da073f72e89a8dce4b600465
Erik Carstensen <erik.carstensen@intel.com>
2022-02-02 19:22:14 +0100

    Represent parameters in object arrays as arrays in vtables
    This is a first step, uses quadratic memory since vtables are still
    expanded across arrays (causes violent death in T_largearray)

--- py/dml/ctree.py
+++ py/dml/ctree.py
@@ -3333,7 +3333,7 @@ def vtable_read(expr):
                                             expr.read())
 
 class TraitParameter(Expression):
-    priority = 160 # SubRef.priority
+    priority = dml.expr.Apply.priority
     @auto_init
     def __init__(self, site, traitref, name, type): pass
 
@@ -3341,8 +3341,11 @@ class TraitParameter(Expression):
         return "%s.%s" % (self.traitref, self.name)
 
     def read(self):
-        return ("%s->%s"
-                % (vtable_read(self.traitref), self.name))
+        t = realtype(self.traitref.ctype())
+        assert isinstance(t, TTrait)
+        vtable_type = f'struct _{cident(t.trait.name)}'
+        return (f'VTABLE_PARAM({self.traitref.read()}, {vtable_type}'
+                f', {self.name})')
 
 class TraitSessionRef(Expression):
     '''A reference to a trait session variable.
