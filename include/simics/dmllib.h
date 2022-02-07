57f347a1603018bf5e8d5517e74040e7f6d4d35f
Erik Carstensen <erik.carstensen@intel.com>
2022-02-07 20:31:01 +0100

    Revert "Allocate trait vtable instances on the heap"
    This reverts commit 9f93a768e0a339bc6bd4ecf7d22c89145351e29a.
    
    Not needed with the upcoming array optimizations.

--- include/simics/dmllib.h
+++ include/simics/dmllib.h
@@ -1047,12 +1047,10 @@ _deserialize_simple_event_data(
 
 // List of vtables of a specific trait in one specific object
 typedef struct {
-        // *base + base_offset is the pointer to the first trait vtable
-        // instance. Instances are stored linearly in a possibly
-        // multi-dimensional array, with outer index corresponding to index of
-        // outer DML object.
-        uintptr_t *base;
-        uint64 base_offset;
+        // first trait vtable instance. Instances are stored linearly in a
+        // possibly multi-dimensional array, with outer index corresponding to
+        // index of outer DML object.
+        uintptr_t base;
         // total number of elements (product of object's dimsizes)
         uint64 num;
         // offset between two vtable instances; at least sizeof(<vtable type>)
