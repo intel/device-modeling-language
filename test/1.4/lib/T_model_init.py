068daf9d246726c63dbfc4e11c6b95f4b957b4c3
Love Waern <love.waern@intel.com>
2022-03-09 10:43:39 +0100

    Address first round of feedback

--- test/1.4/lib/T_model_init.py
+++ test/1.4/lib/T_model_init.py
@@ -9,11 +9,8 @@ def on_model_init(name):
     global model_init_calls
     model_init_calls[name] += 1
 
-stest.expect_equal(model_init_calls['dev'], 0)
-stest.expect_equal(model_init_calls['dev.g'], 0)
+stest.expect_equal(model_init_calls, {'dev': 0, 'dev.g': 0})
 SIM_load_module('dml-test-model_init')
-stest.expect_equal(model_init_calls['dev'], 1)
-stest.expect_equal(model_init_calls['dev.g'], 1)
+stest.expect_equal(model_init_calls, {'dev': 1, 'dev.g': 1})
 obj = SIM_create_object('test', 'obj', [])
-stest.expect_equal(model_init_calls['dev'], 1)
-stest.expect_equal(model_init_calls['dev.g'], 1)
+stest.expect_equal(model_init_calls, {'dev': 1, 'dev.g': 1})
