581bee2efb19cf625f2affd52d76df9d9f0bdc9b
Erik Carstensen <erik.carstensen@intel.com>
2022-03-15 21:51:03 +0100

    Improve test for large vtable arrays

---
+++ test/1.4/structure/T_trait_largearray.py
@@ -0,0 +1,17 @@
+# © 2022 Intel Corporation
+# SPDX-License-Identifier: MPL-2.0
+
+from simicsutils.host import is_windows
+if is_windows():
+    # resource module only available on linux
+    SIM_create_object('test')
+    exit(0)
+
+import resource
+import stest
+
+before = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+SIM_create_object('test', 'obj', [])
+after = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+print(after - before)
+stest.expect_true(after - before < 100)
