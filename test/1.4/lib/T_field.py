1b3ab4243b35d7edbb5327d4f576c71547494c18
Erik Carstensen <erik.carstensen@intel.com>
2022-04-06 13:33:15 +0200

    Repair documentation of field access order, and add test
    Fixes issue #77

--- test/1.4/lib/T_field.py
+++ test/1.4/lib/T_field.py
@@ -7,3 +7,11 @@ for name in ['r1', 'r2', 'r3', 'r4']:
     setattr(obj, 'b_' + name, 4)
     # set adds 1, get adds 2
     stest.expect_equal(getattr(obj, 'b_' + name), 7)
+
+for bank in [obj.bank.le, obj.bank.be]:
+    r = dev_util.Register_LE(bank, 0)
+    r.write(0)
+    # field with least significant bit (i==3) is accessed first
+    stest.expect_equal(bank.r_write_order, 0x03000102)
+    r.read()
+    stest.expect_equal(bank.r_read_order, 0x03000102)
