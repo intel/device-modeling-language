292037cc504eed571d4a9644eaf15024518af909
Henrik Bäärnhielm <henrik.baarnhielm@intel.com>
2022-12-16 13:24:03 +0100

    Run tests in mini-python

--- test/1.4/events/T_after_chk.py
+++ test/1.4/events/T_after_chk.py
@@ -1,9 +1,9 @@
 # © 2021-2023 Intel Corporation
 # SPDX-License-Identifier: MPL-2.0
 
-import sys
 from os.path import join
 import subprocess
+from simicsutils.host import batch_suffix
 
 cpu = SIM_create_object("clock", "clock", [["freq_mhz", 1]])
 obj.queue = cpu
@@ -12,7 +12,8 @@ obj.exec_afters = None
 SIM_write_configuration_to_file("after_chk.chkp", Sim_Save_Nobundle)
 
 subprocess.check_call(
-    [sys.argv[0], "-batch-mode", "-quiet", "-no-copyright", "-core", "-werror",
+    [f'{conf.sim.project}/bin/simics{batch_suffix()}'] +
+    ["-batch-mode", "-quiet", "-no-copyright", "-core", "-werror",
      '-py3k-warnings',
      '-project', conf.sim.project,
      "-L", scratchdir,
