d045a088c40381d996fab8fe22a3c04512f0c890
Henrik Bäärnhielm <henrik.baarnhielm@intel.com>
2023-04-19 10:38:31 +0200

    Avoid old style command line options (SIMICS-20622)
    The new ones were introduced in 6 and old ones are removed or
    deprecated in 7.

--- test/1.2/events/T_dup.py
+++ test/1.2/events/T_dup.py
@@ -13,9 +13,8 @@ SIM_write_configuration_to_file("dup.chkp", Sim_Save_Nobundle)
 
 subprocess.check_call(
     [f'{conf.sim.project}/bin/simics{batch_suffix()}'] +
-    ["-batch-mode", "-quiet", "-no-copyright", "-core", "-werror",
-     '-py3k-warnings',
-     '-project', conf.sim.project,
-     "-L", scratchdir,
-     "-c", "dup.chkp",
-     "-p", join(basedir, "T_dup.cont.py")])
+    ["--batch-mode", "--quiet", "--no-copyright", "--dump-core", "--werror",
+     '--project', conf.sim.project,
+     "--module-path", scratchdir,
+     "-e", "read-configuration dup.chkp",
+     join(basedir, "T_dup.cont.py")])
--- test/1.4/events/T_after_chk.py
+++ test/1.4/events/T_after_chk.py
@@ -13,9 +13,8 @@ SIM_write_configuration_to_file("after_chk.chkp", Sim_Save_Nobundle)
 
 subprocess.check_call(
     [f'{conf.sim.project}/bin/simics{batch_suffix()}'] +
-    ["-batch-mode", "-quiet", "-no-copyright", "-core", "-werror",
-     '-py3k-warnings',
-     '-project', conf.sim.project,
-     "-L", scratchdir,
-     "-c", "after_chk.chkp",
-     "-p", join(basedir, "T_after_chk.cont.py")])
+    ["--batch-mode", "--quiet", "--no-copyright", "--dump-core", "--werror",
+     '--project', conf.sim.project,
+     "--module-path", scratchdir,
+     "-e", "read-configuration after_chk.chkp",
+     join(basedir, "T_after_chk.cont.py")])
