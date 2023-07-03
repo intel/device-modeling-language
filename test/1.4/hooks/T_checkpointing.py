13ec7d7aa72bc038f027a5da1c7893c7e8a40fe7
Erik Carstensen <erik.carstensen@intel.com>
2023-07-03 13:37:29 +0200

    Avoid deprecated option forms
    Not supported with Simics 7

--- test/1.4/hooks/T_checkpointing.py
+++ test/1.4/hooks/T_checkpointing.py
@@ -11,9 +11,8 @@ SIM_write_configuration_to_file("checkpointing.chkp", Sim_Save_Nobundle)
 
 subprocess.check_call(
     [f'{conf.sim.project}/bin/simics{batch_suffix()}'] +
-    ["-batch-mode", "-quiet", "-no-copyright", "-core", "-werror",
-     '-py3k-warnings',
-     '-project', conf.sim.project,
+    ["--batch-mode", "--quiet", "--no-copyright", "--dump-core", "--werror",
+     '--project', conf.sim.project,
      "-L", scratchdir,
      "-c", "checkpointing.chkp",
      "-p", join(basedir, "T_checkpointing.cont.py")])
