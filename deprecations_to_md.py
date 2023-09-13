cff91089bc561d8249d9148cfae6b494fc81a3a7
Erik Carstensen <erik.carstensen@intel.com>
2023-09-13 15:56:02 +0200

    Add documentation for --deprecation

---
+++ deprecations_to_md.py
@@ -0,0 +1,24 @@
+# © 2023 Intel Corporation
+# SPDX-License-Identifier: MPL-2.0
+
+import sys
+from pathlib import Path
+[path_to_dml, header, outfile] = sys.argv[1:]
+sys.path.append(path_to_dml)
+
+from dml.deprecations import deprecations
+from dml.env import api_versions
+
+with open(outfile, 'w') as f:
+    f.write(Path(header).read_text())
+    for (ver, deps) in deprecations.items():
+        if deps and ver in api_versions():
+            f.write(f"### Features available up to --simics-api={ver}\n")
+            f.write("<dl>\n")
+            for d in deps.values():
+                assert d.__doc__
+                f.write(f"  <dt>{d.tag()}</dt>\n")
+                doc = '\n'.join(line[4:] if line.startswith('    ') else line
+                                for line in d.__doc__.strip().splitlines())
+                f.write(f"  <dd>\n\n{doc}\n</dd>\n")
+            f.write("</dl>\n")
