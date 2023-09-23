e7653ac79a289d69d246f23a8b7399564b035aef
Erik Carstensen <erik.carstensen@intel.com>
2023-09-23 22:51:28 +0200

    Use integers to represent API versions
    This is nice because it allows comparisons like "simics_api < 7" to work
    also in Simics 10.
    
    We use the integer 4 to represent the API "4.8"

--- generate_env.py
+++ generate_env.py
@@ -1,7 +1,6 @@
 # © 2021-2023 Intel Corporation
 # SPDX-License-Identifier: MPL-2.0
 
-import os
 import sys
 from pathlib import Path
 
@@ -9,13 +8,14 @@ from simicsutils.host import is_windows
 from simicsutils.internal import api_versions, default_api_version
 
 def generate_env(out):
+    api_versions_dict = {v: 4 if v == '4.8' else int(v) for v in api_versions()}
     Path(out).write_text(f'''\
 def is_windows():
     return {is_windows()}
 def api_versions():
-    return {api_versions()}
+    return {api_versions_dict}
 def default_api_version():
-    return {repr(default_api_version())}
+    return {api_versions_dict[default_api_version()]}
 ''')
 
 if __name__ == '__main__':
