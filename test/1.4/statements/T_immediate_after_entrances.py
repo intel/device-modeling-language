e3289bed00758c72f20a2e6905536ccd9943ea87
Love Waern <love.waern@intel.com>
2024-04-23 16:51:57 +0200

    Immediate after: improve configuration and deletion semantics -- SIMICS-22264
    In particular, this will guarantee that immediate after is suitable for use as
    an `objects_finalized` hook, which it's used as a stand-in for today.

--- test/1.4/statements/T_immediate_after_entrances.py
+++ test/1.4/statements/T_immediate_after_entrances.py
@@ -19,50 +19,71 @@ destroyed_map = {}
 def destroyed(name):
     destroyed_map[name] = destroyed_map.get(name, 0) + 1
 
-obj = simics.SIM_create_object('test', 'obj', queue=cpu)
-stest.expect_equal(obj.count, 0)
-SIM_process_pending_work()
-stest.expect_equal(obj.count, 2)
-
-obj.simple_attr = None
-stest.expect_equal(obj.count, 2)
+obj = simics.SIM_create_object('test', 'obj', queue=cpu, post_inc_attr=None)
+# objects_finalized will execute immediate afters posted in init, post_init,
+# and attribute configuration
+stest.expect_equal(obj.count, 3)
 SIM_process_pending_work()
 stest.expect_equal(obj.count, 3)
+
+obj.simple_attr = None
+stest.expect_equal(obj.count, 3)
+SIM_process_pending_work()
+stest.expect_equal(obj.count, 4)
 
 obj.iface.signal.signal_raise()
-stest.expect_equal(obj.count, 3)
-SIM_process_pending_work()
 stest.expect_equal(obj.count, 4)
+SIM_process_pending_work()
+stest.expect_equal(obj.count, 5)
 
 obj.recursive_entry_attr = None
-stest.expect_equal(obj.count, 4)
-SIM_process_pending_work()
 stest.expect_equal(obj.count, 5)
+SIM_process_pending_work()
+stest.expect_equal(obj.count, 6)
 
 SIM_notify(obj, SIM_notifier_type("static-export"))
-stest.expect_equal(obj.count, 5)
-SIM_process_pending_work()
 stest.expect_equal(obj.count, 6)
+SIM_process_pending_work()
+stest.expect_equal(obj.count, 7)
 
 SIM_notify(obj, SIM_notifier_type("extern-export"))
-stest.expect_equal(obj.count, 6)
-SIM_process_pending_work()
 stest.expect_equal(obj.count, 7)
+SIM_process_pending_work()
+stest.expect_equal(obj.count, 8)
 
 SIM_continue(99999)
-stest.expect_equal(obj.count, 7)
-SIM_continue(1)
 stest.expect_equal(obj.count, 8)
+SIM_continue(1)
+stest.expect_equal(obj.count, 9)
 SIM_continue(99999)
-stest.expect_equal(obj.count, 8)
+stest.expect_equal(obj.count, 9)
 SIM_continue(1)
-stest.expect_equal(obj.count, 9)
-obj.simple_attr = None
-stest.expect_equal(obj.count, 9)
-# destroy() checks that the count has increased to 10
-SIM_delete_object(obj)
+stest.expect_equal(obj.count, 10)
+
+obj.post_never_called = None
+# The immediate after posted by the post_never_called write will be warned
+# about and cancelled
+with stest.expect_log_mgr(obj, 'warning', regex='immediate after'):
+    SIM_delete_object(obj)
+
 stest.expect_equal(destroyed_map.get('dev', 0), 1)
 stest.expect_equal(destroyed_map.get('event', 0), 1)
+
 # Make sure that the delayed deallocation of immediate after state doesn't
 # error or segfault
+SIM_process_pending_work()
+
+# Check that the immediate afters posted during init, post_init, and attribute
+# configuration are cancelled without warning when SIM_add_configuration fails
+# late and has to rollback object creation
+conf.sim.warnings_as_errors = True
+with stest.expect_exception_mgr(SimExc_General):
+    simics.SIM_add_configuration(
+        [pre_conf_object('obj1', 'test', queue=cpu, post_never_called=None),
+         pre_conf_object('obj2', 'test', queue=cpu, error_on_post_init=True)],
+    None)
+
+stest.expect_equal(destroyed_map.get('dev', 0), 3)
+stest.expect_equal(destroyed_map.get('event', 0), 5)
+
 SIM_process_pending_work()
