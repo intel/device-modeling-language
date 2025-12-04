b2a137ae27c65aeacca86893170ba2b95697d28e
Love Waern <love.waern@intel.com>
2025-12-04 14:39:51 +0100

    Placate fisketur

--- deprecations_to_md.py
+++ deprecations_to_md.py
@@ -20,7 +20,7 @@ for feature in compat.features.values():
 with open(outfile, 'w') as f:
     f.write(Path(header).read_text())
     for (ver, features) in sorted(by_version.items()):
-        f.write(rf"""
+        f.write(fr"""
 ### Features available up to and including -\-simics-api={ver.str}
 These features correspond to functionality removed when compiling using
 Simics API {ver.ordinal + 1} or newer. With older Simics API versions, these
